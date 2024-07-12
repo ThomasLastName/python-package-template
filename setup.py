5632ea36caa472c6c6219308b929c0d8beed90c6
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 12:32:35 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+### ~~~
+## ~~~ Originally from https://github.com/maet3608/minimal-setup-py/blob/master/setup.py
+### ~~~ 
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'package_name',
+    version = '0.0.0',
+    url = 'https://github.com/ThomasLastName/python-package-template',
+    author = 'Thomas Winckelman',
+    author_email = 'winckelman@tamu.edu',
+    description = 'Python package template',
+    packages = find_packages(),    
+    install_requires = ["pyreadr"]    # ~~~ when you pip install `package_name`, pip will also install `pyreadr`
+)
