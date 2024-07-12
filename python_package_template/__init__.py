b4aaa26312d07369823b46ab1ca01c3eadab5357
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 14:41:03 -0500

    Update __init__.py

--- python_package_template/__init__.py
+++ python_package_template/__init__.py
@@ -8,6 +8,17 @@ dist = get_distribution('python_package_template')
 __version__ = dist.version
 
 #
+# ~~~ Grab some other metadata including the url
+__metadata__ = dist.get_metadata(dist.PKG_INFO)
+try:
+    for line in __metadata__.splitlines():
+        if line.startswith('Home-page:'):
+            __url__ = line.split(':', 1)[1].strip()
+            break
+except:
+    __url__ = None  # or some default value
+
+#
 # ~~~ Compare local version to latest version
 def see_latest_version():
     import requests, re
@@ -30,8 +41,7 @@ def check_for_update_available(__version__):
         if __version__ < latest_version:
             import warnings
             warnings.warn(
-                f"You are using bnns version {__version__}, but version {latest_version} is available. "
-                f"Consider upgrading using ",
+                f"You are using bnns version {__version__}, but version {latest_version} is available. See {__url__} for more information, including how to upgrade."
                 UserWarning
             )
             print("test")
@@ -40,17 +50,5 @@ def check_for_update_available(__version__):
         warnings.warn(f"Could not check the latest version of bnns: {e}")
 
 check_for_update_available(__version__)
-
-#
-# ~~~ Grab some other metadata including the url
-__metadata__ = dist.get_metadata(dist.PKG_INFO)
-try:
-    for line in __metadata__.splitlines():
-        if line.startswith('Home-page:'):
-            __url__ = line.split(':', 1)[1].strip()
-            break
-except:
-    __url__ = None  # or some default value
-
 
 
