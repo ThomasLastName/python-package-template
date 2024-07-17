3046f1f009a857ced30c0d05292311bfe5bed456
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-17 14:51:24 -0600

    improved comments and removed email

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'python_package_template',
-    version = '0.3.2',
-    url = 'https://github.com/ThomasLastName/python-package-template',
-    author = 'Thomas Winckelman',
-    author_email = 'winckelman@tamu.edu',
-    description = 'Python package template',
-    packages = find_packages(),    
-    install_requires = ["requests"]    # ~~~ when you pip install `python_package_template`, pip will also install `requests`
+    name = 'python_package_template',        # ~~~ NOTE: this must be the same as the name of the folder containing the `__init__.py` file
+    version = '0.3.3',                       # ~~~ replace this
+    url = 'https://github.com/ThomasLastName/python-package-template',   # ~~~ replace this
+    author = 'Thomas Winckelman',            # ~~~ replace this
+    author_email = 'fake_email@gmail.com',   # ~~~ replace this
+    description = 'Python package template', # ~~~ replace this
+    packages = find_packages(),
+    install_requires = ["requests"]          # ~~~ when you pip install `python_package_template`, pip will also install `requests` or whatever else from this list
 )
