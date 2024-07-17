cd80ea0c410e63cd352a8adc93ad2e5e8677550b
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-17 08:58:24 -0600

    Catch for no internet connection. Correct name

--- python_package_template/__init__.py
+++ python_package_template/__init__.py
@@ -1,14 +1,13 @@
-#
-# ~~~ See distribution
-from pkg_resources import get_distribution, DistributionNotFound
-dist = get_distribution('python_package_template')
 
 #
-# ~~~ Fetch local package version
+# ~~~ Fetch local package version from setup.py
+
+from pkg_resources import get_distribution, DistributionNotFound
+dist = get_distribution(__name__)
 __version__ = dist.version
 
 #
-# ~~~ Grab some other metadata including the url
+# ~~~ Grab some other metadata including the package url from setup.py
 __metadata__ = dist.get_metadata(dist.PKG_INFO)
 try:
     for line in __metadata__.splitlines():
@@ -19,11 +18,18 @@ except:
     __url__ = None  # or some default value
 
 #
-# ~~~ Compare local version to latest version
+# ~~~ View the latest version number on the setup.py on github (chat-gpt came up with this one)
 def see_latest_version():
-    import requests, re
-    url = 'https://raw.githubusercontent.com/ThomasLastName/python-package-template/main/setup.py'
-    response = requests.get(url)
+    #
+    # ~~~ Read the contents of the setup.py file
+    try:                                                     # ~~~ try to read the setup.py file on github
+        import requests, re
+        url = "https://raw.githubusercontent.com/ThomasLastName/python-package-template/main/setup.py"
+        response = requests.get(url)
+    except ( requests.ConnectionError, requests.Timeout ):   # ~~~ except don't worry if we don't have an internet connection
+        return None
+    #
+    # ~~~ Parse the contents of the setup.py file
     if response.status_code == 200:
         setup_py_content = response.text
         version_match = re.search(r"version\s*=\s*['\"]([^'\"]+)['\"]", setup_py_content)
@@ -35,6 +41,18 @@ def see_latest_version():
     else:
         raise ValueError(f"Failed to fetch setup.py: {response.status_code} - {response.text}")
 
+#
+# ~~~ Check whether or not internet connection is available
+def we_have_internet():
+    try:
+        import requests
+        response = requests.get( "http://www.google.com", timeout=5 )
+        return True
+    except ( requests.ConnectionError, requests.Timeout ):
+        return False
+
+#
+# ~~~ Compare the local version number with the version number on the setup.py on github
 def check_for_update_available(__version__):
     try:
         latest_version = see_latest_version()
@@ -47,8 +65,6 @@ def check_for_update_available(__version__):
             # print("test")
     except Exception as e:
         import warnings
-        warnings.warn(f"Could not check the latest version of bnns: {e}")
+        warnings.warn(f"Could not check the latest version of {__name__}: {e}")
 
 check_for_update_available(__version__)
-
-
